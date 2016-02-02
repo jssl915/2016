8df6240b7b00d0ec0b987b33371a2a38ff887c04
jssl915 <jssl915@qq.com>
2016-02-02 17:33:31 +0800

    作品集

---
+++ works/2/js/html5.js
@@ -0,0 +1,6 @@
+(function() {
+    var a = ['section', 'article', 'nav', 'header', 'footer' /* 其他HTML5元素 */];
+    for (var i = 0, j = a.length; i < j; i++) {
+        document.createElement(a[i]);
+    }
+})();
